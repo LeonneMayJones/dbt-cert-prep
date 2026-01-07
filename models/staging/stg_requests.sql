a740f84cc2b7529a59e2f75f7d971fe350eddb1d
Leonne Jones <leonnepony@gmail.com>
2026-01-07 01:58:10 +0000

    Day1ExercisesDone

--- models/staging/stg_requests.sql
+++ models/staging/stg_requests.sql
@@ -1,5 +1,6 @@
 select 
     unique_key as request_id, 
     complaint_description, 
-    created_date as created_at
+    created_date as created_at, 
+    status
 from {{source('austin_311','311_service_requests')}}
